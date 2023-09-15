d83709d203ddd351c5749b19e71e722a6f150cbf
Gishu_gg <girish2001gupta@gmail.com>
2023-09-15 21:19:08 +0530

    Refactor: Descriptive Url Names

--- TwilioServer/callhandlingAPI/urls.py
+++ TwilioServer/callhandlingAPI/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('abc', views.answer_call, name='answer_call'),
+    path('answer-call', views.answer_call, name='answer_call'),
     path('handle-recording', views.handle_recording, name='handle_recording'),
 ]
