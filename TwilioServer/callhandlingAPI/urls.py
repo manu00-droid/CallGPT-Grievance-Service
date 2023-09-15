c3a763774ce23a55542ab73c1923080ef94a21f4
manu00-droid <manavsidana00@gmail.com>
2023-09-15 18:58:10 +0530

    feat: call recording feature added

--- TwilioServer/callhandlingAPI/urls.py
+++ TwilioServer/callhandlingAPI/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('abc', views.answer_call, name='answer_call'),
+    path('handle-recording', views.handle_recording, name='handle_recording'),
 ]
--- TwilioServer/callhandlingAPI/views.py
+++ TwilioServer/callhandlingAPI/views.py
@@ -1,24 +1,44 @@
 from rest_framework.decorators import api_view
-# from rest_framework.response import Response
 from django.http import HttpResponse
 from twilio.twiml.voice_response import VoiceResponse
+import twilio.rest
+import requests
+from django.http import JsonResponse
 
-# Create your views here.
+asid = "ACc773938691c59d16d13b0b8dacb3d461"
+auth = "51a05de4dac2fc81dc56d51432bbc641"
+client = twilio.rest.Client(asid, auth)
 
 
 @api_view(['POST'])
 def answer_call(request):
-    """Respond to incoming phone calls with a brief message."""
-    # Start our TwiML response
-    resp = VoiceResponse()
-    print("hello world")
-    print(resp)
-    print(type(resp))
 
-    # Read a message aloud to the caller
-    resp.say("Thank you for calling! Have a great day.", voice='Polly.Amy')
-    # resp.play("https://api.twilio.com/classic.mp3")
-    # return str(resp)
+    resp = VoiceResponse()
+    resp.say("Thank you for calling! Please leave your message after the tone and press 9 to end the call.")
+    resp.record(
+        action='/handle-recording',
+        method='POST',
+        maxLength='180',
+        timeout='5',
+        finishOnKey='9'
+    )
+
+    return HttpResponse(
+        str(resp), content_type='application/xml; charset=utf-8'
+    )
+
+
+@api_view(['POST'])
+def handle_recording(request):
+    recording_url = request.POST.get("RecordingUrl")
+
+    resp = VoiceResponse()
+
+    if recording_url:
+        resp.say("Thank you for your message. The recording has been received.")
+    else:
+        resp.say("No recording received.")
+
     return HttpResponse(
         str(resp), content_type='application/xml; charset=utf-8'
     )
