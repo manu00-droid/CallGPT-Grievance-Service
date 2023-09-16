ff894c3a9a8d3cd40a08c5eccc80c8a048f307c0
adityaagg7 <79642133+adityaagg7@users.noreply.github.com>
2023-09-16 14:54:14 +0530

    feat: translate api working

--- apisoolver/apisolve/urls.py
+++ apisoolver/apisolve/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("parse", views.parse_unstructured_text, name="parse_name"),
     path("recording", views.get_recording_url, name="get_reccording_url"),
-
+    path("translate",views.translate,name="translate_text"),
 ]
--- apisoolver/apisolve/views.py
+++ apisoolver/apisolve/views.py
@@ -20,32 +20,36 @@ OAKEY=os.environ.get("OAKEY")
 openai.api_key = OAKEY
 asid=os.environ.get("asid")
 atoken=os.environ.get("auth_token")
+message_url=os.environ.get("message_url")
+DBURL=os.environ.get("DBURL")
 
 ic(asid,atoken)
 client = twilio.rest.Client(asid, atoken)
 
-
-def transcribe():
-
-    audio_file = open("/media/naman/7FFD-CD8E/excrclr/Hackathons/sih23/grievance-solver/apisoolver/apisolve/abc3.mp3", "rb")
+def transcribe(path,pno):
+    # path=request.POST.get('url')
+    audio_file = open("path", "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    print("I AM HERE")
+    # print("I AM HERE")
     print(transcript)
+    parse_unstructured_text(transcript,pno)
 
 
-@api_view(['POST'])
-def parse_unstructured_text(request):
-    input_text=request.POST.get('data')
+
+
+def parse_unstructured_text(transcript,phone_number):
+    input_text=transcript
     ic(input_text)
     
     
     prompt = f"""
             Following text is a transcript of a call recording from a client who wants to file a complaint which can be lodged to
             either electricity or water department of the caller's state government.
-
-            from the text given below, extract the following fields and parse it into a json.
+            and also given a phone number, add it to the json as well.
+            from the text given below, extract the following fields and parse it into a json, after converting them to english alphabets.
             
             Input transcript: {input_text}
+            phone Number:{phone_number}
             
             Fields:
             - client_name
@@ -54,6 +58,7 @@ def parse_unstructured_text(request):
             - address (dict of full address, city, state,pincode(if available)))
             - adhaar card number (12 digits)
             - phone number (10 digits)
+            - language used by caller
             
             Valid JSON Output, omit fields that are not present:
               """
@@ -76,11 +81,60 @@ def parse_unstructured_text(request):
         j = None
         
     
-    ic(j,type(j))
-    return HttpResponse(json.dumps(j), content_type="application/json")
+    # ic(j,type(j))
+    requests.post(DBURL,json.dumps(j))
+    return HttpResponse()
 
 @api_view(['POST'])
 def get_recording_url(request):
+    path="a"
     r_url=request.POST.get('url')
     ic(r_url)
     requests.get(r_url)
+    phone_number=request.POST.get('phone_no')
+    transcribe(path,phone_number)
+
+
+
+@api_view(['POST'])
+def translate(request):
+    j=request.POST
+    text=f"Hi {j['name']}, your complaint has been registered with the {j['department']} department, with complaint id {j['cid']}. Here is the description of your complaint:\n {j['complaintDescription']}"
+    prompt = f"""
+            Following text is a data of the Name of a person,his phone number,  and a Complaint-ID, also a language is given,the department of the complain, and a brief complaint description. 
+
+            Translate the data into the language specified in the text, and return the translation as text with the same structure as before, just change the language.
+            
+            Input text: {text}
+            Language to be chages into : {j['language']}
+            
+            Valid text Output:
+              """
+    messages=[{"role": "system","content":prompt}]
+
+    # response = openai.ChatCompletion.create(
+    #   					model="gpt-3.5-turbo", 
+    #   					messages=messages, 
+    #   					temperature=0, 
+    #   					max_tokens=3000,
+    #                     top_p=1, 
+    #   					frequency_penalty=0, 
+    #   					presence_penalty=0
+    				# )
+    # print(response)
+
+    # try:
+    #     k = json.loads(response["choices"][0]["message"]["content"])
+    # except json.decoder.JSONDecodeError:
+    #     k=None
+    k=text
+    print(k)
+
+    phno=j['phoneNumber']
+    req_body={
+        "phone_no":phno,
+        "body":k
+    }
+    requests.post(message_url,req_body)
+    return HttpResponse()
+
