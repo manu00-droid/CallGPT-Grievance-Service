b5bc84d3f68d9fa28691e3f61afd7e9f81fee10a
manu00-droid <manavsidana00@gmail.com>
2023-09-17 12:23:08 +0530

    fix: remove @requestbody annotation inside the registerComplaint method due to contenttype error

--- database/src/main/java/com/callgpt/database/controller/DatabaseController.java
+++ database/src/main/java/com/callgpt/database/controller/DatabaseController.java
@@ -1,5 +1,6 @@
 package com.callgpt.database.controller;
 
+import com.callgpt.database.configuration.SmsServiceConfig;
 import com.callgpt.database.dto.RegisterComplaintDTO;
 import com.callgpt.database.entity.Complaint;
 import com.callgpt.database.repository.ComplaintRepository;
@@ -27,24 +28,25 @@ import java.util.Optional;
 public class DatabaseController {
     private UserService userService;
     private ComplaintService complaintService;
+    private static SmsServiceConfig smsServiceConfig;
 
     @Autowired
-    public DatabaseController(UserService userService, ComplaintService complaintService) {
+    public DatabaseController(UserService userService, ComplaintService complaintService, SmsServiceConfig smsServiceConfig) {
         this.userService = userService;
         this.complaintService = complaintService;
+        this.smsServiceConfig = smsServiceConfig;
     }
 
-    private static final String BASE_URI = "http://127.0.0.1:8083/translate/";
-    private static WebClient webClient = WebClient.create(BASE_URI);
+//    private static WebClient webClient = WebClient.create(smsServiceConfig.getSmsServiceUrl());
 
     @PostMapping("/register-complaint")
-    public ResponseEntity<String> registerComplaint(@RequestBody RegisterComplaintDTO request) {
-        String name = request.getName();
-        String phoneNumber = request.getPhoneNumber();
-        String aadhaarNumber = request.getAadhaarNumber();
+    public ResponseEntity<String> registerComplaint(RegisterComplaintDTO request) {
+        String name = request.getClient_name();
+        String phoneNumber = request.getPhone_number();
+        String aadhaarNumber = request.getAadhaar_card_number();
         String city = request.getCity();
         String state = request.getState();
-        String complaintDescription = request.getComplaintDescription();
+        String complaintDescription = request.getComplaint_description();
         String department = request.getDepartment();
         String language = request.getLanguage();
         try {
@@ -54,14 +56,18 @@ public class DatabaseController {
             Complaint complaint = new Complaint(user, complaintDescription, department);
             complaint = complaintService.save(complaint);
             //call msg service
+            System.out.println("calling sms service");
             DatabaseController.callSmsService(complaint, user);
+            System.out.println("returning response to the llm");
             return ResponseEntity.ok("User and Complaint saved successfully.");
         } catch (Exception e) {
+            System.out.println(e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error saving data.");
         }
     }
 
     private static void callSmsService(Complaint complaint, User user) {
+        WebClient webClient = WebClient.create(smsServiceConfig.getSmsServiceUrl());
         MultiValueMap<String, String> bodyValues = new LinkedMultiValueMap<>();
         bodyValues.add("cid", String.valueOf(complaint.getCid()));
         bodyValues.add("name", user.getName());
